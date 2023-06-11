3dc2113a1597a8479a07b526b7bc52c6579925e0
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-06-11 13:04:57 +0900

    fork, wait but timeout

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#ifndef USERPROG_PROCESS_H
-#define USERPROG_PROCESS_H
-
-#include "threads/thread.h"
-
-tid_t process_create_initd (const char *file_name);
-tid_t process_fork (const char *name, struct intr_frame *if_);
-int process_exec (void *f_name);
-// int process_exec (char **argv, int argc);
-int process_wait (tid_t);
-void process_exit (void);
-void process_activate (struct thread *next);
-
-#endif /* userprog/process.h */
-=======
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
@@ -25,6 +9,6 @@ int process_exec (void *f_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
+struct thread *get_child_thread(int pid);
 
 #endif /* userprog/process.h */
->>>>>>> f0bf602606fbae06622f8a813b8f72c5c95965f0
